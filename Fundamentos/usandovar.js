60a14fbb4cfb06481af2e6012db118ae21c55d93
Gabriel <gabriel.rteodoro@yahoo.com.br>
2024-02-19 19:31:02 -0300

    feature

---
+++ Fundamentos/usandovar.js
@@ -0,0 +1,11 @@
+{{{{var sera = 'será?'}}}}
+
+console.log(sera)
+
+function teste() {
+    var local = 1234
+}
+
+teste()
+console.log(local) // o Escopo da variavel é dentro da função apenas
+
---
+++ Fundamentos/usandovar2.js
@@ -0,0 +1,7 @@
+var number = 1
+{
+    var number = 2
+    console.log('dentro =', number)
+}
+
+console.log('fora = ',number)
